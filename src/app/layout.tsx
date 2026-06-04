72b38c07137f86198781060b139a7701251e79a5
Programmer Rakibul <rakibul00206@gmail.com>
2026-06-04 21:25:15 +0600

    feat: add theming support with next-themes and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
+import { cn } from "@/lib/utils";
+import ProviderWrapper from "@/providers/provider-wrapper";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Oxanium } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
-const geistMonoHeading = Geist_Mono({subsets:['latin'],variable:'--font-heading'});
+const geistMonoHeading = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-heading",
+});
 
-const oxanium = Oxanium({subsets:['latin'],variable:'--font-sans'});
+const oxanium = Oxanium({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +34,21 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", oxanium.variable, geistMonoHeading.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        oxanium.variable,
+        geistMonoHeading.variable,
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <ProviderWrapper>
+          <main className="min-h-dvh space-y-8 md:space-y-16">{children}</main>
+        </ProviderWrapper>
+      </body>
     </html>
   );
 }
